c0da90b8451230fa7807b510a80108a353edf590
ishma12 <np03cs4m210008@heraldcollege.np>
2023-11-02 20:26:54 +0545

    Certain change in CSS

--- logout.js
+++ logout.js
@@ -4,7 +4,7 @@ function confirmLogout() {
     var result = confirm("Are you sure you want to logout?");
     if (result) {
         // Redirect to the logout page or perform logout action
-        window.location.href = "/landingpage/index.html"; // Replace with your actual logout page URL
+        window.location.href = "../landingpage/index.html"; // Replace with your actual logout page URL
     } else {
         // User chose not to logout, do nothing
     }
